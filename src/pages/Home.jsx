13d2744cee08bcbfdf42c27f40fa69bab63c124d
Frank Perez <21010185+fperez028@users.noreply.github.com>
2025-07-08 20:30:58 +0000

    updated Home.jsx with simple card layout list view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,78 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import React, { useEffect } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { initializeData } from "../store.js";
+import { useNavigate } from "react-router-dom";
+import { Card } from "../components/Card.jsx";
+import { HorizontalScroll } from "../components/HorizontalScroll.jsx";
 
 export const Home = () => {
+	const { store, dispatch } = useGlobalReducer();
+	const navigate = useNavigate();
 
-  const {store, dispatch} =useGlobalReducer()
+	useEffect(() => {
+		if (
+			store.people.length === 0 &&
+			store.planets.length === 0 &&
+			store.starships.length === 0
+		) {
+			initializeData(dispatch);
+		}
+	}, []);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="container-fluid px-4">
+			<h1 className="text-white mt-4 mb-5 text-center">Star Wars Blog</h1>
+
+			{store.isLoading ? (
+				<p className="text-white text-center">Loading data...</p>
+			) : store.error ? (
+				<p className="text-danger text-center">{store.error}</p>
+			) : (
+				<>
+					<HorizontalScroll title="Characters">
+						{store.people.map((char) => (
+							<Card
+								key={char.uid}
+								title={char.name}
+								subtitleLines={[
+									`Gender: ...`,
+									`Hair: ...`,
+									`Eyes: ...`
+								]}
+								onClick={() => navigate(`/character/${char.uid}`)}
+							/>
+						))}
+					</HorizontalScroll>
+
+					<HorizontalScroll title="Planets">
+						{store.planets.map((planet) => (
+							<Card
+								key={planet.uid}
+								title={planet.name}
+								subtitleLines={[
+									`Population: ...`,
+									`Terrain: ...`
+								]}
+								onClick={() => navigate(`/planet/${planet.uid}`)}
+							/>
+						))}
+					</HorizontalScroll>
+
+					<HorizontalScroll title="Starships">
+						{store.starships.map((ship) => (
+							<Card
+								key={ship.uid}
+								title={ship.name}
+								subtitleLines={[
+									`Model: ...`,
+									`Maker: ...`
+								]}
+								onClick={() => navigate(`/starship/${ship.uid}`)}
+							/>
+						))}
+					</HorizontalScroll>
+				</>
+			)}
 		</div>
 	);
-}; 
+};
